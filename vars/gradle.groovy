0715421ff185642f730692470e7afc258e32cb8d
luismella <luismellareyes@gmail.com>
2022-01-29 12:07:11 -0300

    libreria

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -1,58 +1,51 @@
-/*
-	forma de invocación de método call:
-	def ejecucion = load 'script.groovy'
-	ejecucion.call()
-*/
 def call(){
-    env.STAGE = "Paso 1: Compliar"
-    stage("$env.STAGE "){
-        sh "mvn clean compile -e"
+    //Escribir directamente el código del stage, sin agregarle otra clausula de Jenkins.
+    stage("Paso 1: Build && Test"){
+        sh "echo 'Build && Test!'"
+        sh "gradle clean build"
+        // code
     }
-    env.STAGE = "Paso 2: Testear"
-    stage("$env.STAGE "){    
-        sh "mvn clean test -e"
-    }
-    stage("Paso 3: Build .Jar"){
-        sh "mvn clean package -e"
-    }
-    stage("Paso 4: Sonar - Análisis Estático"){
+    stage("Paso 2: Sonar - Análisis Estático"){
         sh "echo 'Análisis Estático!'"
-        withSonarQubeEnv('sonarqube') {
-            sh 'mvn clean verify sonar:sonar -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build'
+        withSonarQubeEnv('sonarqube3') {
+            sh "echo 'Calling sonar by ID!'"
+            // Run Maven on a Unix agent to execute Sonar.
+            sh './gradlew sonarqube -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build'
         }
     }
-    stage("Paso 5: Curl Springboot Gradle sleep 20"){
+    stage("Paso 3: Curl Springboot Gradle sleep 20"){
         sh "gradle bootRun&"
-        sh "sleep 60 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
-    }
-    stage("Paso 6: Subir Nexus"){
-        nexusPublisher nexusInstanceId: 'nexus',
-                nexusRepositoryId: 'devops-usach',
-                packages: [
-                        [$class: 'MavenPackage',
-                         mavenAssetList: [
-                                 [classifier: '',
-                                  extension: '.jar',
-                                  filePath: 'build/DevOpsUsach2020-0.0.1.jar'
-                                 ]
-                         ],
-                         mavenCoordinate: [
-                                 artifactId: 'DevOpsUsach2020',
-                                 groupId: 'com.devopsusach2020',
-                                 packaging: 'jar',
-                                 version: '0.0.1'
-                         ]
-                        ]
-                ]
-    }
-    stage("Paso 7: Descargar Nexus"){
-        sh ' curl -X GET -u $NEXUS_USER:$NEXUS_PASSWORD "http://nexus:8081/repository/devops-usach-nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar" -O'
-    }
-    stage("Paso 8: Levantar Artefacto Jar"){
-        sh 'nohup bash java -jar DevOpsUsach2020-0.0.1.jar & >/dev/null'
-    }
-    stage("Paso 9: Testear Artefacto - Dormir(Esperar 20sg) "){
         sh "sleep 20 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
     }
+    stage("Paso 4: Subir Nexus"){
+        nexusPublisher nexusInstanceId: 'nexus',
+        nexusRepositoryId: 'devops-usach-nexus',
+        packages: [
+            [$class: 'MavenPackage',
+                mavenAssetList: [
+                    [classifier: '',
+                    extension: '.jar',
+                    filePath: 'build/libs/DevOpsUsach2020-0.0.1.jar'
+                ]
+            ],
+                mavenCoordinate: [
+                    artifactId: 'DevOpsUsach2020',
+                    groupId: 'com.devopsusach2020',
+                    packaging: 'jar',
+                    version: '0.0.1'
+                ]
+            ]
+        ]
+    }
+    stage("Paso 5: Descargar Nexus"){
+        sh ' curl -X GET -u $NEXUS_USER:$NEXUS_PASSWORD "http://nexus:8081/repository/devops-usach-nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar" -O'
+    }
+    stage("Paso 6: Levantar Artefacto Jar"){
+        sh 'nohup bash java -jar DevOpsUsach2020-0.0.1.jar & >/dev/null'
+    }
+    stage("Paso 7: Testear Artefacto - Dormir(Esperar 20sg) "){
+        sh "sleep 60 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
+    }
 }
+
 return this;
